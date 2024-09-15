16b14ad44acd38f74a1109d01f2c02208e216ba5
EnzoDev999 <Innocxnt999@gmail.com>
2024-09-15 20:14:31 +0200

    env change

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { io } from "socket.io-client";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react";
 
-const socket = io("http://localhost:5000/", {
+const socket = io("https://yourduel.onrender.com/", {
   transports: ["websocket", "polling"], // Assurez-vous d'utiliser WebSocket
 });
 
